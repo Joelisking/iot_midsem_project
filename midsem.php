3cbe117759027e5e46f3d6ff9d5dfc90274d60b4
Joelisking <93987931+Joelisking@users.noreply.github.com>
2022-10-31 14:17:13 +0000

    Final midsem.php

--- midsem.php
+++ midsem.php
@@ -2,14 +2,14 @@
 $servername="localhost";
 $username="root";
 $password="";    
-$dbname="iot_midsem";
+$dbname="midsem_proj";
 
 $DistanceCm=$_GET['DistanceCm'];
 // $LDRValue=$_GET['LDRValue'];
 // $Temperature=$_GET['Temperature'];
 $con = mysqli_connect($servername,$username,$password,$dbname);
 
-$sql = "INSERT INTO waterreading (WaterLevel) VALUES ('{$DistanceCm}')";
+$sql = "INSERT INTO tanks (water_level, owner_id) VALUES ('{$DistanceCm}', '1')";
 
 if (mysqli_query($con, $sql)){
 echo "New record created successfully";
